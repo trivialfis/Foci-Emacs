72947a0642f4a5982aeed9562b4bde7035541f49
fis <ybbs.daans@hotmail.com>
2017-07-27 07:30:41 +0800

    better slime configuration

--- langs/lisp-trivialfis.el
+++ langs/lisp-trivialfis.el
@@ -1,4 +1,4 @@
-;;; lisp-trivialfis --- Summary
+;;; lisp-trivialfis --- Summary -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 (require 'company)
@@ -7,15 +7,22 @@
 
 (defun trivialfis/lisp ()
   "Configuration for clisp."
-  (setq inferior-lisp-program "sbcl")
-  (setq slime-company-completion 'fuzzy)
-  (add-to-list 'slime-contribs 'slime-fancy)
-  (add-to-list 'slime-contribs 'slime-company)
+  (setq inferior-lisp-program "/usr/bin/sbcl"
+	slime-company-completion 'fuzzy
+	slime-contribs '(slime-fancy
+			 slime-company
+			 slime-scratch
+			 slime-editing-commands))
   (add-to-list 'company-backends 'company-slime)
 
   (flycheck-mode 1)
 
+  (define-key slime-prefix-map (kbd "C-c h o") 'slime-documentation-lookup)
+  (define-key slime-mode-map (kbd "C-c C-a") 'slime-compile-file)
   (slime-setup)
-  (slime-mode 1))
+  (slime-mode 1)
+  (unless (slime-connected-p)
+    (slime)))
+
 (provide 'lisp-trivialfis)
 ;;; lisp-trivialfis ends here
