e804a24bdef67cabc6b0ee4903652ade8b5bc743
fis <ybbs.daans@hotmail.com>
2017-10-03 05:11:26 +0800

    Add virtualenv support for python

--- langs/python-trivialfis.el
+++ langs/python-trivialfis.el
@@ -1,32 +1,44 @@
-;;; python-trivialfis --- Summary
+;;; python-trivialfis --- Python configuration
 ;;; Commentary:
 ;;; Code:
 
 (require 'elpy)
 (require 'company)
+(require 'helm-xref)
+(require 'f)
 ;; (require 'lsp-mode)
 ;; (require 'lsp-python)
 
-(defun lsp-client()
-  "Not ready to use."
-  ;; "LSP, configuration files confliction might happen."
-  (lsp-mode))
+(defun trivialfis/activate-virtualenv ()
+  "Find and activate virtualenv."
+  (let ((env-path (f-traverse-upwards
+		   (lambda (path)
+		     (or (equal path (f-expand "~"))
+			 (f-exists? (f-join path "bin/activate")))))))
+    (when (and env-path
+	       (not (equal env-path (f-expand "~"))))
+      (pyvenv-activate env-path))))
 
-(defun elpy-setup()
+(defun trivialfis/elpy-setup()
   "Elpy configuration."
   (flycheck-mode 1)
-  (elpy-mode)
+  (elpy-enable)
+  (elpy-mode 1)
   (with-eval-after-load 'elpy
     (setq elpy-rpc-python-command "python3"
-  	  python-shell-interpreter "python3")
+  	  python-shell-interpreter "python3"
+	  elpy-rpc-backend "jedi")
     ;; (elpy-use-ipython)
     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)) ;; Replace flymake with flycheck
-    ;; (add-hook 'elpy-mode-hook 'flycheck-mode)
-    (add-to-list 'company-backends 'elpy-company-backend)))
+    (add-to-list 'company-backends 'elpy-company-backend))
+  (setq xref-show-xrefs-function 'helm-xref-show-xrefs))
 
 (defun trivialfis/python()
-  ;; (lsp-client)
-  (elpy-setup)
+  "Python configuration."
+  ;; lsp is not ready.
+  ;; (lsp-mode)
+  (trivialfis/elpy-setup)
+  (trivialfis/activate-virtualenv)
   )
 
 (provide 'python-trivialfis)
