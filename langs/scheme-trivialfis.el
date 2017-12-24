79467614f44c69cf9e084f58d30cb49fbbea470b
fis <ybbs.daans@hotmail.com>
2017-12-24 20:45:14 +0800

    Add guix to the load path

--- langs/scheme-trivialfis.el
+++ langs/scheme-trivialfis.el
@@ -2,10 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'geiser-guile)
 (defun trivialfis/scheme ()
   "Run Geiser."
   (save-window-excursion
-    (run-geiser 'guile)))
+    (run-geiser 'guile))
+  (with-eval-after-load 'geiser-guile
+    (add-to-list 'geiser-guile-load-path "~/Workspace/guix")))
 
 
 (provide 'scheme-trivialfis)
