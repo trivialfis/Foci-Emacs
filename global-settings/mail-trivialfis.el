edc928234cb9b9c81fdfabd2ea7839c461817de4
fis <ybbs.daans@hotmail.com>
2018-01-15 18:42:43 +0800

    Add mail spliter

--- global-settings/mail-trivialfis.el
+++ global-settings/mail-trivialfis.el
@@ -2,6 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 (require 'gnus)
+(require 'nnimap)
 (require 'smtpmail)
 
 (defun trivialfis/mail()
@@ -18,6 +19,14 @@
 
   (setq smtpmail-smtp-server "smpt-mail.outlook.com"
 	smtpmail-smtp-service 587)
+
+  (setq nnimap-split-methods
+	'(
+	  ;; ("guix")
+	  ;; ("guix:devel")
+	  ("guix:debug" "List-Id:.*\n.*<guix-devel.gnu.org>")
+	  ("guix:patch" "List-Id:.*<guix-patches.gnu.org>")
+	  ("guix:help" "^To:.*help-guix@gnu.org|Cc:.*help-guix@gnu.org|List-ID:.*\n.*<guix-devel.gnu.org>")))
 
   ;; (eval-and-compile
   ;;   (add-to-list 'load-path "~/.guix-profile/share/emacs/site-lisp/"))
