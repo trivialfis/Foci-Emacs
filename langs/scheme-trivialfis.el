1c38a4b8a87440267b22ca1c1a8b178d4aa5879d
fis <ybbs.daans@hotmail.com>
2018-01-22 05:45:04 +0800

    langs/scheme-trivialfis.el: Add kbds.

--- langs/scheme-trivialfis.el
+++ langs/scheme-trivialfis.el
@@ -3,20 +3,24 @@
 ;;; Code:
 
 (require 'geiser)
+(require 'geiser-mode)
 (require 'geiser-repl)
 (require 'geiser-guile)
+(require 'geiser-compile)
+
 (defun trivialfis/scheme ()
   "Run Geiser."
-  (with-eval-after-load 'geiser
-    (setq geiser-active-implementations '(guile)))
-  (save-window-excursion
-    (run-geiser 'guile))
-  ;; (with-eval-after-load 'geiser-repl
+  (setq geiser-active-implementations '(guile))
   (setq geiser-repl-query-on-kill-p nil)
-  (with-eval-after-load 'geiser-guile
-    (add-to-list 'geiser-guile-load-path "~/Workspace/guix"))
-  )
 
+  (define-key geiser-mode-map (kbd "C-c C-a") 'geiser-compile-current-buffer)
+  (define-key geiser-mode-map (kbd "C-c C-k") 'geiser-mode-switch-to-repl-and-enter)
+  (eval-after-load 'geiser
+    (save-window-excursion
+      (run-geiser 'guile))
+    )
+
+  (add-to-list 'geiser-guile-load-path "~/Workspace/guix"))
 
 (provide 'scheme-trivialfis)
 ;;; scheme-trivialfis.el ends here
