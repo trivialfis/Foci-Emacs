b6b293ff1712c6629741d28f5a0a013f113e12c2
fis <ybbs.daans@hotmail.com>
2018-01-28 10:40:33 +0800

    global-settings: Generalize foci-project mode.
    * global-settings/foci-org-projects.el: Improve usability.
    Directly embed query string.
    Add unread mails count.
    Move mu4e config to starting of mode.

--- global-settings/foci-org-projects.el
+++ global-settings/foci-org-projects.el
@@ -4,20 +4,15 @@
 
 (require 'mail-trivialfis)
 (require 'subr-x)
+(require 'map)
 (require 'mu4e)
 (require 'org)
 
-(defvar %project-template
-  "
-*
-** Home Page:
-** Mailing Lists:
+(defvar *project-template*
+  '("* " "** Home Page: \n" "** Mailing List:\n\n"
+    "** Synopsis:\n\n" "** Description:\n\n" "** Source dir: "))
 
-** Synopsis:
-** Description:
-
-** Source dir:
-")
+(defvar %mu4e-search "mu4e-headers-search")
 
 (defun project-come-back (buffer-name)
   "Come back to BUFFER-NAME."
@@ -28,39 +23,90 @@
 (defun goto-list(addr &optional use-msgid)
   "Show mailing list ADDR.
 If USE-MSGID is t, then use message-id rather than list-id."
-  (trivialfis/mu4e-config)
-  (define-key mu4e-headers-mode-map
-    (kbd "q")
-    (project-come-back (buffer-name)))
   (if use-msgid
       (mu4e-headers-search (string-join `("msgid:" ,addr)))
     (mu4e-headers-search (string-join `("list:" ,addr)))))
+
+(defun get-query-at-line ()
+  "Get the query string at current line."
+  (beginning-of-line)
+  (let ((query-start (search-forward "\""))
+	(query-end (progn
+		     (end-of-line)
+		     (search-backward "\""))))
+    (buffer-substring-no-properties query-start query-end)))
+
+(defun update-unread-at-line()
+  "Update the numbers of unread mails with point at current line."
+  (interactive)
+  (let* ((query-command (get-query-at-line))
+	 (count (string-trim
+		 (shell-command-to-string (string-join
+					   `("mu find flag:unread AND "
+					     ,query-command
+					     " | wc -l"))))))
+    (search-forward "Unread")
+    (search-forward-regexp "\([0-9]*\)")
+    (message (match-string 0))
+    (replace-match (string-join `("(" ,count ")")))))
+
+(defun goto-unread ()
+  "Go to unread messages view."
+  (trivialfis/mu4e-config)
+  (let ((query-command (get-query-at-line)))
+    (print query-command)
+    (mu4e-headers-search (string-join `("flag:unread AND "
+					,query-command)))))
+
+(defun construct-query-link (query desc)
+  "Construct link for a mailing list.
+QUERY: The query string for mu4e,
+DESC: Mailing list description."
+  (let* ((list-link (string-join
+		     `("+ [[elisp:(" ,%mu4e-search " \"" ,query "\")]["
+		       ,desc "]]")))
+	 (unread "Unread(0)")
+	 (unread-link (string-join `("[[elisp:(goto-unread)][" ,unread "]]")))
+	 (link-width (string-width desc))
+	 (unread-width (string-width unread))
+	 (blank-num (- 75 link-width unread-width))
+	 (blanks (make-string blank-num ?\xB7)))
+    (print blanks)
+    (string-join `("   " ,list-link ,blanks ,unread-link))))
+
+(defun insert-list ()
+  "Insert mailing list at current line."
+  (interactive)
+  (let ((query (read-string "Query: "))
+	(desc (read-string "List Description: ")))
+    (when (not (= (line-beginning-position) (point)))
+      (end-of-line)
+      (insert "\n"))
+    (insert (construct-query-link query desc))))
 
 (defun insert-list-id ()
   "Insert mailing list at current line.
 Prompt for List-ID and list name"
   (interactive)
   (let ((addr (read-string "List ID:"))
-	(name (read-string "List name:")))
+	(name (read-string "List Description:")))
     (org-indent-line)
-    (insert (string-join
-	     `("+ [[elisp:(progn (goto-list \""
-	       ,addr "\"))][" ,name "]]")))))
+    (insert (construct-query-link (string-join `("list:" ,addr)) name))))
 
 (defun insert-message-id ()
   "Insert message id for mailing list."
   (interactive)
   (let ((addr (read-string "Message Id:"))
-	(name (read-string "List name:")))
+	(name (read-string "List Description:")))
     (org-indent-line)
-    (insert (string-join
-	     `("+ [[elisp:(progn (goto-list \""
-	       ,addr "\" t))][" ,name "]]")))))
+    (insert (construct-query-link (string-join `("msgid:" ,addr)) name))))
 
 (defun insert-project ()
   "Insert a project template."
   (interactive)
-  (insert %project-template))
+  (let ((name (read-string "Project name: ")))
+    (insert (string-join `(,(car *project-template*) ,name "\n"))))
+  (cl-mapcar 'insert (cdr *project-template*)))
 
 (defun foci-update-mails ()
   "Call mu4e for updating mails."
@@ -70,6 +116,9 @@ Prompt for List-ID and list name"
 (defun foci-project-turn-on ()
   "Turn foci-project-mode on."
   (trivialfis/mu4e-config)
+  (define-key mu4e-headers-mode-map
+    (kbd "q")
+    (project-come-back (buffer-name)))
   (add-to-list 'org-link-frame-setup '(file . find-file))
   (flyspell-mode 0))
 
