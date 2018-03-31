1847037cf9975fa029264dc8ff575c85bd8504e1
fis <ybbs.daans@hotmail.com>
2018-03-31 03:24:44 -0400

    Merge mail pass.

--- global-settings/offlineimap.py
+++ global-settings/offlineimap.py
@@ -2,4 +2,4 @@ from subprocess import check_output
 
 
 def mailpasswd():
-    return check_output("gpg2 -dq ~/.imap.gpg", shell=True).strip("\n")
+    return check_output("gpg2 -dq ~/.authinfo.gpg", shell=True).strip("\n").split(' ')[-1]
