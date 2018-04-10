083c7e57f34ea62b7b6870df0728905ee700eb83
fis <ybbs.daans@hotmail.com>
2018-04-11 05:14:43 +0800

    Scheme: Add implementation.

--- langs/scheme-trivialfis.el
+++ langs/scheme-trivialfis.el
@@ -28,15 +28,14 @@
 
 (defun trivialfis/scheme ()
   "Run Geiser."
-  (setq geiser-active-implementations '(guile))
+  (setq geiser-active-implementations '(guile racket chicken))
   (setq geiser-repl-query-on-kill-p nil)
 
   (define-key geiser-mode-map (kbd "C-c C-a") 'geiser-compile-current-buffer)
   (define-key geiser-mode-map (kbd "C-c C-k") 'geiser-mode-switch-to-repl-and-enter)
-  (eval-after-load 'geiser
-    (save-window-excursion
-      (run-geiser 'guile))
-    )
+  ;; (eval-after-load 'geiser
+  ;;   (save-window-excursion
+  ;;     (run-geiser 'guile)))
 
   (add-to-list 'geiser-guile-load-path "~/.config/guix/latest"))
 
