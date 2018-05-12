dba7b01df416508d02c7e68f0f1adf051b042811
fis <ybbs.daans@hotmail.com>
2018-05-12 14:21:46 +0800

    Enable lsp-ui for java.

--- langs/java-trivialfis.el
+++ langs/java-trivialfis.el
@@ -3,12 +3,21 @@
 ;;; Code:
 (require 'lsp-java)
 (require 'lsp-mode)
+;; (require 'company-lsp)
+(require 'lsp-ui)
 (require 'programming-trivialfis)
 
 (defun trivialfis/java ()
   "Java configuration."
   ;; Don't know why prog mode isn't loaded.
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  (c-set-style "gnu")
+  (setq lsp-java--workspace-folders (list "/home/fis/Workspace/bitwar"))
   (trivialfis/programming-init)
+  (flycheck-mode 1)
+  ;; (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  ;; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  ;; (push 'company-lsp company-backends)
   (lsp-java-enable))
 
 (provide 'java-trivialfis)
