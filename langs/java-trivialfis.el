7725490ef87f6f8929a7e2a84c9c4b6349217b7a
fis <ybbs.daans@hotmail.com>
2018-08-14 19:57:45 +0800

    Remove lsp-jave for now.

--- langs/java-trivialfis.el
+++ langs/java-trivialfis.el
@@ -1,7 +1,7 @@
 ;;; java-trivialfis.el --- Summary
 ;;; Commentary:
 ;;; Code:
-(require 'lsp-java)
+;; (require 'lsp-java)
 (require 'lsp-mode)
 ;; (require 'company-lsp)
 (require 'lsp-ui)
@@ -13,13 +13,14 @@
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   (c-set-style "gnu")
   (setq lsp-before-save-edits nil)
-  (setq lsp-java--workspace-folders (list "/home/fis/Workspace/bitwar"))
+  ;; (setq lsp-java--workspace-folders (list "/home/fis/Workspace/bitwar"))
   (trivialfis/programming-init)
   (flycheck-mode 1)
   ;; (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   ;; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   ;; (push 'company-lsp company-backends)
-  (lsp-java-enable))
+  ;; (lsp-java-enable)
+  )
 
 (provide 'java-trivialfis)
 ;;; java-trivialfis.el ends here
