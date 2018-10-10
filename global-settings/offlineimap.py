90739fafded1d1fc9fdb5b27489bd66a6b74992e
fis <ybbs.daans@hotmail.com>
2018-10-10 22:55:11 +1300

    Reverse gpg order.

--- global-settings/offlineimap.py
+++ global-settings/offlineimap.py
@@ -7,7 +7,7 @@ def mailpasswd():
     '''Entry point.'''
     try:
         return check_output(
-            "gpg -dq ~/.authinfo.gpg", shell=True).strip("\n").split(' ')[-1]
+            "gpg2 -dq ~/.authinfo.gpg", shell=True).strip("\n").split(' ')[-1]
     except subprocess.CalledProcessError:
         return check_output(
-            "gpg2 -dq ~/.authinfo.gpg", shell=True).strip("\n").split(' ')[-1]
+            "gpg -dq ~/.authinfo.gpg", shell=True).strip("\n").split(' ')[-1]
