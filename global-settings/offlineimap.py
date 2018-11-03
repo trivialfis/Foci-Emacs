58a7907b4b3a3c5886155914b886ab72a668c7d3
fis <ybbs.daans@hotmail.com>
2018-11-03 17:35:34 +1300

    Add finding address to OfflineImap entry function.

--- global-settings/offlineimap.py
+++ global-settings/offlineimap.py
@@ -1,13 +1,26 @@
 '''OfflineImap gpg reading.'''
-from subprocess import check_output
-import subprocess
+from subprocess import check_output, CalledProcessError
 
 
-def mailpasswd():
+def mailpasswd(address):
     '''Entry point.'''
+    def get_account_pass(accounts):
+        '''Return the password from correct account.'''
+        for account in accounts:
+            if account.find(address) != -1:
+                current_account = account
+                break
+        current_account = current_account.split(' ')
+        password = current_account[-1]
+        return password
+
     try:
-        return check_output(
-            "gpg2 -dq ~/.authinfo.gpg", shell=True).strip("\n").split(' ')[-1]
-    except subprocess.CalledProcessError:
-        return check_output(
-            "gpg -dq ~/.authinfo.gpg", shell=True).strip("\n").split(' ')[-1]
+        all_accounts = check_output(
+            "gpg2 -dq ~/.authinfo.gpg", shell=True).strip("\n").split('\n')[-1]
+        password = get_account_pass(all_accounts)
+        return password
+    except CalledProcessError:
+        all_accounts = check_output(
+            "gpg -dq ~/.authinfo.gpg", shell=True).strip("\n").split('\n')
+        password = get_account_pass(all_accounts)
+        return password
