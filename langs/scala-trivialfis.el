1b057c0b8d71bb28759c17029de969e4c33c3e35
fis <jm.yuan@outlook.com>
2020-01-03 15:27:22 +0800

    Fix Scala.

--- langs/scala-trivialfis.el
+++ langs/scala-trivialfis.el
@@ -3,17 +3,18 @@
 ;;; Code:
 
 (use-package programming-trivialfis)
-(require 'lsp-trivialfis)
+(use-package lsp-trivialfis)
 (use-package company-lsp)
 
 ;; Super slow at initialization.
 (defun trivialfis/scala ()
   "Configuration for scala."
-  (trivialfis/lsp)
   (trivialfis/programming-init)
+  (trivialfis/lsp)
   (lsp)
   (lsp-ui-mode)
-  (flycheck-mode 1))
+  (flycheck-mode 1)
+  (yas-minor-mode))
 
 (provide 'scala-trivialfis)
 ;;; scala-trivialfis.el ends here
