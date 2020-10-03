bd2f4e973a9d5ddd2529cc10e3bdeef84894e83c
fis <jm.yuan@outlook.com>
2020-10-03 23:46:03 +0800

    Add JSON config.

---
+++ langs/json-model.el
@@ -0,0 +1,11 @@
+;;; json-model.el --- Summary
+;;; Commentary:
+;;; Code:
+
+(require 'json-mode)
+
+(defun trivialfis/json ()
+  "JSON configuration."
+  (setq-default js-indent-leve 2))
+(provide 'json-model)
+;;; json-model.el ends here
