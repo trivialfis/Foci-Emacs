460dd40c0de2950783a2537e31bd80c4ece0f6c4
jiamingy <jm.yuan@outlook.com>
2020-11-22 04:31:50 -0500

    Set frame size.

--- early-init.el
+++ early-init.el
@@ -1,2 +1,9 @@
 (custom-set-variables
  '(custom-enabled-themes (quote (misterioso))))
+(setq default-frame-alist
+      '(
+        (tool-bar-lines . 0)
+        (width . 128) ; chars
+        (height . 32) ; lines
+        (left . 30)
+        (top . 30)))
