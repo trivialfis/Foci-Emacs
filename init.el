91ff0c9f5e30c6ae40667e602841e9476e5dc3f9
fis <jm.yuan@outlook.com>
2021-04-30 12:10:28 +0800

    Remove comment.

--- init.el
+++ init.el
@@ -76,10 +76,7 @@
   ((member "Segoe UI Emoji" (font-family-list)) "Segoe UI Emoji")
   ((member "Symbola" (font-family-list)) "Symbola")
   ((member "Apple Color Emoji" (font-family-list)) "Apple Color Emoji"))
- ;; Apple Color Emoji should be before Symbola, but Richard Stallman disabled it.
- ;; GNU Emacs Removes Color Emoji Support on the Mac
  ;; http://ergoemacs.org/misc/emacs_macos_emoji.html
- ;;
  )
 
 ;; (toggle-debug-on-error)
