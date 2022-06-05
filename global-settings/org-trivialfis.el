6eb40e224850d17b2b1db67c6bcc47f5235c8b03
fis <jm.yuan@outlook.com>
2022-06-05 21:14:33 +0800

    Org roam.

--- global-settings/org-trivialfis.el
+++ global-settings/org-trivialfis.el
@@ -130,5 +130,23 @@ Minor mode for using org and mu4e to manage projects (Foci playground).
 
 \(fn)" t nil))
 
+(setq org-directory (concat (getenv "HOME") "/Workspace/roam/"))
+
+(use-package org-roam
+  :after org
+  :init (setq org-roam-v2-ack t) ;; Acknowledge V2 upgrade
+  :custom
+  (org-roam-directory (file-truename org-directory))
+  :config
+  (org-roam-setup)
+  :bind (("C-c n f" . org-roam-node-find)
+         ("C-c n r" . org-roam-node-random)
+         (:map org-mode-map
+               (("C-c n i" . org-roam-node-insert)
+                ("C-c n o" . org-id-get-create)
+                ("C-c n t" . org-roam-tag-add)
+                ("C-c n a" . org-roam-alias-add)
+                ("C-c n l" . org-roam-buffer-toggle)))))
+
 (provide 'org-trivialfis)
 ;;; org-trivialfis ends here
