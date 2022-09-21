1d83c92bb6702735bbbc10e4b3e8d85801fa5b50
fis <jm.yuan@outlook.com>
2022-09-22 06:17:41 +0800

    Company mode.

--- global-settings/programming-trivialfis.el
+++ global-settings/programming-trivialfis.el
@@ -88,7 +88,16 @@ MODE: the major programming mode"
   (local-set-key (kbd "TAB") 'company-indent-or-complete-common)
   ;; Quickhelp
   (autoload 'trivialfis/company-quickhelp "quickhelp-trivialfis")
-  (add-hook 'company-mode-hook 'trivialfis/company-quickhelp))
+  (add-hook 'company-mode-hook 'trivialfis/company-quickhelp)
+  (add-hook 'company-mode-hook #'(lambda ()
+				   (require 'color)
+				   (let ((bg (face-attribute 'default :background)))
+				     (custom-set-faces
+				      `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+				      `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+				      `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+				      `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+				      `(company-tooltip-common ((t (:inherit font-lock-constant-face)))))))))
 
 (autoload 'auto-update-file-header "header2")
 (defun trivialfis/programming-post ()
