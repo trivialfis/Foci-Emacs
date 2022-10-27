e781996c23af55f536123a54435fc80a0f5e7930
fis <jm.yuan@outlook.com>
2022-10-27 21:07:15 +0800

    remove workaround.

--- init.el
+++ init.el
@@ -170,8 +170,7 @@
   :defer t
   :commands (trivialfis/replace-completion-region
 	     trivialfis/replace-completing-read
-	     trivialfis/replace-read-file
-	     helm-find-files))
+	     trivialfis/replace-read-file))
 (let ((helm-commands '(helm-M-x
 		       helm-find-files
 		       helm-buffers-list
