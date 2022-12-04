1898ad5b4de330cf650deeee57e96b5d959f0ed3
jiamingy <jm.yuan@outlook.com>
2022-12-04 14:20:42 +0800

    Remove ecb.

--- global-settings/programming-trivialfis.el
+++ global-settings/programming-trivialfis.el
@@ -27,14 +27,6 @@
   ;; Following line is not needed if use-package.el is in ~/.emacs.d
   (require 'use-package))
 
-(use-package ecb
-  :init (setf ecb-fix-window-size 'width
-	      ;; ecb-minor-mode-text "Ecb"
-	      ecb-windows-width 0.18)
-  :defer t
-  :commands (ecb-activate)
-  :config (message "ECB loaded."))
-
 (use-package semantic/idle
   :commands (semantic-mode
 	     semanticdb-enable-gnu-global-databases)
