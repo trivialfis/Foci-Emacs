fbeffcbda56fd1930b7f8522888f49e10e9b62c0
Jiaming Yuan <jm.yuan@outlook.com>
2024-08-06 15:22:57 +0800

    Gnome 46.

--- global-settings/vterm.py
+++ global-settings/vterm.py
@@ -8,28 +8,61 @@ and install `python3-nautilus` on Ubuntu.  Restart nautilus service if necessary
 
 import os
 import subprocess
+from typing import List
 from urllib.parse import unquote, urlparse
 
 from gi.repository import GObject, Nautilus
 
+version = Nautilus._version.split(".")
 
-class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
-    def __init__(self):
-        pass
+if int(version[0]) >= 4:
 
-    def menu_activate_cb(self, menu, f):
-        parsed = urlparse(f.get_uri())
-        path = unquote(parsed.path)
-        cmd = f'emacs --chdir="{path}" --eval="(trivialfis/vterm)"'
-        if f.is_directory():
-            subprocess.Popen(cmd, shell=True)
+    class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
+        def menu_activate_cb(
+            self, menu: Nautilus.MenuItem, f: Nautilus.FileInfo
+        ) -> None:
+            parsed = urlparse(f.get_uri())
+            path = unquote(parsed.path)
+            cmd = f'emacs --chdir="{path}" --eval="(trivialfis/vterm)"'
+            if f.is_directory():
+                subprocess.Popen(cmd, shell=True)
 
-    def get_background_items(self, window, file):
-        item = Nautilus.MenuItem(
-            name="MenuProvider::VTerm",
-            label="Open in VTerm",
-            tip="Open VTerm here.",
-            icon="",
-        )
-        item.connect("activate", self.menu_activate_cb, file)
-        return (item,)
+        def get_background_items(
+            self, current_folder: Nautilus.FileInfo
+        ) -> List[Nautilus.MenuItem]:
+            item = Nautilus.MenuItem(
+                name="MenuProvider::VTerm",
+                label="Open in VTerm",
+                tip="Open VTerm here.",
+                icon="",
+            )
+            item.connect("activate", self.menu_activate_cb, current_folder)
+            return [
+                item,
+            ]
+
+else:
+
+    class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
+        def menu_activate_cb(
+            self, menu: Nautilus.MenuItem, f: Nautilus.FileInfo
+        ) -> None:
+            parsed = urlparse(f.get_uri())
+            path = unquote(parsed.path)
+            cmd = f'emacs --chdir="{path}" --eval="(trivialfis/vterm)"'
+            if f.is_directory():
+                subprocess.Popen(cmd, shell=True)
+
+        def get_background_items(
+            self, window: Nautilus.FileInfo, f
+        ) -> List[Nautilus.MenuItem]:
+            item = Nautilus.MenuItem(
+                name="MenuProvider::VTerm",
+                label="Open in VTerm",
+                tip="Open VTerm here.",
+                icon="",
+            )
+            item.connect("activate", self.menu_activate_cb, f)
+            return [
+                item,
+            ]
