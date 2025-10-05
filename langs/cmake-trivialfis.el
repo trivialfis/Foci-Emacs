cf8fd5294d556f3c5fceb9dcd3d77833c7977aad
Jiaming Yuan <jm.yuan@outlook.com>
2025-10-05 09:24:46 +0800

    CMake.

--- langs/cmake-trivialfis.el
+++ langs/cmake-trivialfis.el
@@ -1,4 +1,4 @@
-;;; cmake-trivialfis.el --- Summary
+;;; cmake-trivialfis.el --- Summary -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Code:
 (require 'lsp)
@@ -16,10 +16,21 @@
   :multi-root t
   :server-id 'cmake-lsp))
 
+(use-package company-cmake
+  :defines company-cmake-executable)
+
 (defun trivialfis/cmake ()
   "CMake configuration."
   ;; ctags -e -R --languages=CMake
-  )
+  ;; (require 'company-cmake)
+
+  ;; Locate CMake
+  (if (and (not company-cmake-executable)
+	   (string= system-type "windows-nt"))
+      (let* ((bin-path "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\CommonExtensions\\Microsoft\\CMake\\CMake\\bin")
+	     (path (locate-file "cmake" (list bin-path) exec-suffixes 'executable)))
+	(if path
+	    (setq company-cmake-executable path)))))
 
 (provide 'cmake-trivialfis)
 ;;; cmake-trivialfis.el ends here
