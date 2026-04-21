00fe41021cd3dbffed68dcc94c2ea94b05b5452e
Jiaming Yuan <jm.yuan@outlook.com>
2026-04-22 01:37:27 +0800

    cmds.

--- global-settings/agent-shell-cursor-acp.el
+++ global-settings/agent-shell-cursor-acp.el
@@ -55,7 +55,7 @@ The first element is the command name, and the rest are command parameters."
     "git show" "git log" "git diff" "git status" "git stash" "git branch --list"
     "gh run view" "gh issue view" "gh pr view"
     "cd" "wc" "head" "tail" "grep" "sed" "ruff" "cat" "ls" "echo" "touch"
-    "xargs" "sort" "find" "sort"
+    "xargs" "sort" "find" "sort" "rg" "which"
     "mvn"
     "curl"
     "testxgboost"))
