bcc668a441352a14299c121d28098177863ebbb9
Jiaming Yuan <jm.yuan@outlook.com>
2026-04-29 03:05:56 +0800

    indent.

--- langs/alloy-trivialfis.el
+++ langs/alloy-trivialfis.el
@@ -9,9 +9,46 @@
 
 (use-package programming-trivialfis)
 
+(defvar treesit-simple-indent-rules)
+
+(defvar trivialfis/alloy--indent-rules
+  '((alloy
+     ;; Closing delimiters must be tested before container rules; otherwise
+     ;; fact/pred/fun block closing braces indent as body expressions.
+     ((node-is "}") parent-bol 0)
+     ((node-is "]") parent-bol 0)
+     ((node-is ")") parent-bol 0)
+     ;; Top-level: no indent
+     ((parent-is "source_file") column-0 0)
+     ;; `sig_body' starts at the first field, so anchor sibling fields to the
+     ;; enclosing signature instead of to the previous field.
+     ((parent-is "sig_body") grand-parent alloy-ts-mode-indent-offset)
+     ;; Inside blocks/braces: indent
+     ((parent-is "block_expression") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "set_comprehension") parent-bol alloy-ts-mode-indent-offset)
+     ;; Parameters
+     ((parent-is "decl_list") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "expr_list") parent-bol alloy-ts-mode-indent-offset)
+     ;; General continuation
+     ((parent-is "sig_decl") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "pred_decl") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "fun_decl") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "fact_decl") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "assert_decl") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "command") parent-bol alloy-ts-mode-indent-offset)
+     ((parent-is "enum_decl") parent-bol alloy-ts-mode-indent-offset)
+     ;; Default: no change
+     (no-node parent-bol 0)))
+  "Local indentation rules for `alloy-ts-mode'.")
+
+(defun trivialfis/alloy--configure-indentation ()
+  "Install local tree-sitter indentation rules for Alloy."
+  (setq-local treesit-simple-indent-rules trivialfis/alloy--indent-rules))
+
 (use-package alloy-ts-mode
   :defer t
-  :commands alloy-ts-mode)
+  :commands alloy-ts-mode
+  :hook (alloy-ts-mode . trivialfis/alloy--configure-indentation))
 
 (use-package lsp-trivialfis
   :autoload trivialfis/lsp)
